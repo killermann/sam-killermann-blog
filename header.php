b9573122bb7ff925bc6477962abec7185bd005d9
Sam Killermann <sK@hues.xyz>
2018-03-13 15:52:58 -0500

    added tracking script

--- header.php
+++ header.php
@@ -21,6 +21,16 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-45231959-1"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'UA-45231959-1');
+	</script>
+
 </head>
 
 <body <?php body_class('color0'); ?>>

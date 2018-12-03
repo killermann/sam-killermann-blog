8ba1493c0ff9717895e74074138f08675092769d
Sam Killermann <sK@hues.xyz>
2018-12-03 13:41:11 -0600

    fix all articles page header

--- page-articles.php
+++ page-articles.php
@@ -10,11 +10,54 @@ get_header(); ?>
 	<main id="main" class="site-main">
 
 		<?php
-		while ( have_posts() ) : the_post();
+		while ( have_posts() ) : the_post();?>
 
-			get_template_part( 'template-parts/content', 'page' );
 
-		endwhile;?>
+		<article id="post-<?php the_ID(); ?>" <?php post_class('bigpad wrap'); ?>>
+			<header class="entry-header">
+				<?php the_title( '<h1 class="page-title">', '</h1>' ); ?>
+			</header><!-- .entry-header -->
+
+			<div class="post-thumbnail chameleon-bg">
+				<?php the_post_thumbnail('full');?>
+			</div>
+
+			<div class="entry-content type-wrap">
+				<?php
+					the_content();
+
+					wp_link_pages( array(
+						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'sam-killermann-blog' ),
+						'after'  => '</div>',
+					) );
+				?>
+			</div><!-- .entry-content -->
+
+			<?php if ( get_edit_post_link() ) : ?>
+				<footer class="entry-footer">
+					<?php
+						edit_post_link(
+							sprintf(
+								wp_kses(
+									/* translators: %s: Name of current post. Only visible to screen readers */
+									__( 'Edit <span class="screen-reader-text">%s</span>', 'sam-killermann-blog' ),
+									array(
+										'span' => array(
+											'class' => array(),
+										),
+									)
+								),
+								get_the_title()
+							),
+							'<span class="edit-link">',
+							'</span>'
+						);
+					?>
+				</footer><!-- .entry-footer -->
+			<?php endif; ?>
+		</article><!-- #post-<?php the_ID(); ?> -->
+
+		<?php endwhile;?>
 
 		<section class="loop">
 			<?php

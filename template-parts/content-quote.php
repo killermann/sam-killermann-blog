f2403eb687d5c957b0b46f87a074248efeef7311
Sam Killermann <sK@hues.xyz>
2018-12-03 00:25:08 -0600

    fix quote permalink display on home

--- template-parts/content-quote.php
+++ template-parts/content-quote.php
@@ -39,7 +39,7 @@
 		<div class="grid-cell">
 			<?php sam_killermann_blog_entry_footer(); ?>
 		</div>
-		<?php if( ! is_singular()) : ?>
+		<?php if( ! is_single()) : ?>
 		<div class="grid-cell grid-cell--autoSize">
 			<a href="<?php the_permalink(); ?>">
 				Permalink &rarr;

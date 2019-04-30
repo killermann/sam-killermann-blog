1160092e40ef093581abdadab3a0ffac66fac078
Sam Killermann <sK@hues.xyz>
2019-04-30 09:37:16 -0500

    more clear link styles

--- template-parts/content-link.php
+++ template-parts/content-link.php
@@ -8,25 +8,23 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class('wrap'); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('wrap chameleon-border'); ?>>
 
 	<header class="entry-header">
 		<?php
-		sam_killermann_blog_primary_category();
-		echo ' | ';
 		if ( is_single() ) :
-			echo '<span class="primary-category"><a href="' . esc_url( home_url() ) . '/type/link" rel="bookmark">Link</a></span>';
+			echo '<span class="primary-category chameleon-color"><a href="' . esc_url( home_url() ) . '/type/link" rel="bookmark">Link</a></span>';
 		else :
-			echo '<span class="primary-category">Link</span>';
+			echo '<span class="primary-category chameleon-color">Link</span>';
 		endif;
 		$content = get_the_content();
 		$has_url = get_url_in_content($content);
 		$link = $has_url ? $has_url : apply_filters('the_permalink', get_permalink());
 
 		if ( is_single() ) :
-			the_title( '<h1 class="entry-title"><a title="Visit ' . $link .'" href="' . $link . '" target="_blank">', ' &rarr;</a></h1>' );
+			the_title( '<h1 class="entry-title"><a title="Visit ' . $link .'" href="' . $link . '" target="_blank">', ' <span class="chameleon-color">&rarr;</span></a></h1>' );
 		else :
-			the_title( '<h2 class="h1 entry-title"><a title="Visit ' . $link .'" href="' . $link . '" target="_blank">', ' &rarr;</a></h2>' );
+			the_title( '<h2 class="entry-title"><a title="Visit ' . $link .'" href="' . $link . '" target="_blank">', ' <span class="chameleon-color">&rarr;</span></a></h2>' );
 		endif;
 		if ( has_excerpt() ) :
 			echo '<p class="excerpt">';
@@ -34,7 +32,6 @@
 			echo '</p>';
 		endif;
 		sam_killermann_blog_posted_on();
-		sam_killermann_blog_posted_by();
 		sam_killermann_blog_post_thumbnail();
 
 		?>
@@ -48,5 +45,8 @@
 	<footer class="entry-footer">
 		<?php sam_killermann_blog_entry_footer();?>
 	</footer>
+	<?php if ( !is_single() ){ ?>
+		<a class="absolute-link" href="<?php echo $link ?>" title="<?php echo the_title();?>" rel="bookmark"></a>
+	<?php }  ?>
 
 </article><!-- #post-<?php the_ID(); ?> -->
